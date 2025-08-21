1973fc119121dbd9ba1cbc973fe2cbc566781671
Diogo Moura <diogoodev@gmail.com>
2025-08-20 23:52:13 -0300

    Atualiza o projeto para .NET 9.0 e aprimora a lógica de jogo de dados com mensagens de vitória e bônus para resultados duplicados e triplicados.

--- 01_Console_Basics/01_Logica_IF_ELSE/Program.cs
+++ 01_Console_Basics/01_Logica_IF_ELSE/Program.cs
@@ -12,6 +12,10 @@ int roll1 = dice.Next(1,7);
 int roll2 = dice.Next(1,7);
 int roll3 = dice.Next(1,7);
 
+//teste
+//roll1 = 3;
+//roll2 = 3;
+//roll3 = 1;
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
@@ -19,27 +23,47 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 //resultados duplicados
 if ((roll1 == roll2) || (roll3 == roll2) || (roll1 == roll3))
 {
-  Console.WriteLine("You rolled doubles! +2 bonus to total!");
-  total += 2;
+  //resultados triplicados
+  //5 == 5 &&        5    ==    5
+  if ((roll1 == roll2) && (roll2 == roll3)) // if aninhado
+  {
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+  }
+  else
+  {
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+  }
 }
 
-//resultados triplicados
-//5 == 5 &&        5    ==    5
-if ((roll1 == roll2) && (roll2 == roll3))
+// if (total >= 15)
+// {
+//   Console.WriteLine("You Win!");
+// }
+// else
+// {
+//   Console.WriteLine("Sorry, you lose.");
+// }
+if (total >= 16)
 {
-  Console.WriteLine("You rolled triples! +6 bonus to total!");
-  total += 6;
+  Console.WriteLine("You won a new car!");
+}
+else if (total >= 10)
+{
+  Console.WriteLine("You won a new laptop!");
+}
+else if (total == 7)
+{
+  Console.WriteLine("You won a trip!");
+}
+else
+{
+  Console.WriteLine("You won a Cat!");
 }
 
-if (total >= 15)
-{
-  Console.WriteLine("You Win!");
-}
 
-if ( total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
+
 
 // // Expressão booliana em métodos
 
@@ -71,6 +95,13 @@ bool result = message.("dog");
 if (price) {
   Console.WriteLine("Dolar detectado");
  } */
+
+/*#Exercício – Criar lógica de decisão aninhada com if, elseif e else
+Concluido
+*/
+
+// #Exercício – Concluir uma atividade de desafio que aplica regras de negócios - Em andamento, bem no inicio.
+
 
 
 
